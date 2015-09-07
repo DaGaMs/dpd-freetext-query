62bede00384fed99c7dcf71912f993b885d038b4
ivan <ivan@evershelfapp.com>
2015-09-07 18:40:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var Resource = require('deployd/lib/resource')
+    , Script = require('deployd/lib/script')
+    , util = require('util')
+    , debug = require('debug')('dpd-freetext-query')
+    , request = require('request')
+    , parseString = require('xml2js').parseString;
+
+function FreetextResource() {
+    Resource.apply(this, arguments);
+}
+util.inherits(FreetextResource, Resource);
+
+FreetextResource.label = "Freetext-Query";
+FreetextResource.events = ["get"];
+
+module.exports = FreetextResource;
+
+FreetextResource.prototype.clientGeneration = true;
+
+FreetextResource.prototype.handle = function(ctx, next) {
+    var parts = ctx.url.split('/').filter(function(p) {
+        return p;
+    });
+
+    var slf = this;
+    var domain = {
+        url: ctx.url,
+        req_url: ctx.req.url,
+        parts: parts,
+        query: ctx.query,
+        body: ctx.body,
+        requestFactory: function () { return request; },
+        respond: function(result) {
+            ctx.done(null, result);
+        },
+        xmlParser: { parseString: parseString }
+    };
+    if (ctx.method === "GET" && this.events.get) {
+        this.events.get.run(ctx, domain, function(err) {
+            if (err) ctx.done(err);
+        });
+    } else {
+        next();
+    }
+};
